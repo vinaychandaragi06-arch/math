9777696cbadbc33d1a7180ab44861ec07e4e7907
vinaychandaragi06-arch <vinaychandaragi06@gmail.com>
2026-01-19 14:42:26 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,5 +1,12 @@
-a = int(input("enter first number: "))
-b = int(input("enter second number: "))
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 
-print(f"addition: {a + b}")
-print(f"subtraction: {a - b}")
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+
+if b != 0:
+    print(f"Division: {a / b}")
+else:
+    print("Division: Cannot divide by zero")
+
